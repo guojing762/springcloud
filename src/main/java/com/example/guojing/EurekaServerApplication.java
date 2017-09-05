0aa346008700fa7180daa24a0de5abdbd7d335c7
lemon <guojingandmeng@gmail.com>
2017-09-05 17:16:49 +0800

    1

--- src/main/java/com/example/guojing/EurekaServerApplication.java
+++ src/main/java/com/example/guojing/EurekaServerApplication.java
@@ -16,5 +16,6 @@ public class EurekaServerApplication {
 	public static void main(String[] args) {
 		new SpringApplicationBuilder(EurekaServerApplication.class)
 				.web(true).run(args);
+		System.out.println("1");
 	}
 }

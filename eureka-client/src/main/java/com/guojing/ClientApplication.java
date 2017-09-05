00b1ed73e65403497e25e84615af39e7f65b3cd6
lemon <guojingandmeng@gmail.com>
2017-09-05 19:01:27 +0800

    my

---
+++ eureka-client/src/main/java/com/guojing/ClientApplication.java
@@ -0,0 +1,17 @@
+package com.guojing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ClientApplication {
+
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(
+				ClientApplication.class)
+				.web(true).run(args);
+	}
+}

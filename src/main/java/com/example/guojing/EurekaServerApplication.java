4b200f46009d40a0c84f72ffd3803eadc3fcfe1e
lemon <guojingandmeng@gmail.com>
2017-09-05 17:01:45 +0800

    eureka

--- src/main/java/com/example/guojing/EurekaServerApplication.java
+++ src/main/java/com/example/guojing/EurekaServerApplication.java
@@ -2,11 +2,19 @@ package com.example.guojing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaServerApplication {
 
-	public static void main(String[] args) {
+	public static void main1(String[] args) {
 		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(EurekaServerApplication.class)
+				.web(true).run(args);
 	}
 }

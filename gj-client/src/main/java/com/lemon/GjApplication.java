099e239d7a9fc179f9d57b765e32a0e532a05285
lemon <guojingandmeng@gmail.com>
2017-09-06 17:21:34 +0800

    yy

---
+++ gj-client/src/main/java/com/lemon/GjApplication.java
@@ -0,0 +1,12 @@
+package com.lemon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GjApplication.class, args);
+	}
+}

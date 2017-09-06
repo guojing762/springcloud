caf71743f1cf999c5910758cb7270b6ac97786a8
lemon <guojingandmeng@gmail.com>
2017-09-06 17:19:00 +0800

    cl

---
+++ consul-client/src/main/java/com/lemon/GuojingApplication.java
@@ -0,0 +1,15 @@
+package com.lemon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuojingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuojingApplication.class, args);
+	}
+	public static void main1(String[] args) {
+		SpringApplication.run(GuojingApplication.class, args);
+	}
+}

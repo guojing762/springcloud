a26228c119e70942266b94daa31e2eb0fdbd1553
lemon <guojingandmeng@gmail.com>
2017-09-07 16:36:16 +0800

    自定义输入

--- spring-batch/src/main/java/com/guojing/SpringBatchApplication.java
+++ spring-batch/src/main/java/com/guojing/SpringBatchApplication.java
@@ -1,4 +1,20 @@
 package com.guojing;
+/**
+ * 1、批处理定期提交。
+ 2、并行批处理：并行处理工作。
+ 3、企业消息驱动处理
+ 4、大规模的并行处理
+ 5、手动或是有计划的重启
+ 6、局部处理：跳过记录（如：回滚）
+ 技术目标：
+ 1、利用Spring编程模型：使程序员专注于业务处理，让Spring框架管理流程。
+ 2、明确分离批处理的执行环境和应用。
+ 3、提供核心的，共通的接口。
+ 4、提供开箱即用（out of the box）的简单的默认的核心执行接口。
+ 5、提供Spring框架中配置、自定义、和扩展服务。
+ 6、所有存在的核心服务可以很容的被替换和扩展，不影响基础层。
+ 7、提供一个简单的部署模式，利用Maven构建独立的Jar文件。
+ */
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- spring-batch/src/test/java/com/guojing/SpringBatchApplicationTests.java
+++ spring-batch/src/test/java/com/guojing/SpringBatchApplicationTests.java
@@ -1,13 +1,46 @@
 package com.guojing;
 
+import com.guojing.entity.Person;
+import com.guojing.process.PersonItemProcessor;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringBatchApplicationTests {
+
+	//查询
+	private static final String GET_PRODUCT = "select * from person where personName = ?";
+
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
+	@Test
+	public void testq(){
+		List<Person> personList = jdbcTemplate.query(GET_PRODUCT, new Object[] {"2"}, new RowMapper<Person>() {
+			@Override
+			public Person mapRow(ResultSet resultSet, int rowNum ) throws SQLException {
+				Person p = new Person();
+				p.setPersonName(resultSet.getString(1));
+				p.setPersonAge(resultSet.getString(2));
+				p.setPersonSex(resultSet.getString(3));
+				return p;
+			}
+		});
+		Assert.assertNotNull(personList);
+	}
 
 	@Test
 	public void contextLoads() {
